6efde0c8613c5f22013eb3c5afa57bb20c86347d
brynnebursack <brynnebursack@gmail.com>
2018-06-20 16:02:45 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,20 @@
+# student_one = "Caroline"
+# student_two = "Millie"
+# student_three = "Sam"
+# student_four = "Sonali"
+
+# puts "In this class, we have a student named #{student_four}."
+
+# student_list = ["Caroline","Millie","Sam","Sonali"]
+# puts student_list
+
+# friends = ["Ross","Joey","Chandler","Monica","Phoebe","Rachel"]
+# puts friends
+
+disney = ["Moana","Remy","Belle","Woody","Merida"]
+ages = [16, 16, 15]
+books = ["Looking for Alaska","The Hate U Give","Carry On"]
+books.push("Geometry Textbook")
+puts ages.length
+
+
